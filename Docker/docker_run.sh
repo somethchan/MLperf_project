4b154ad49c62cf0dfd17e68dc46bf18f972a5948
somethchan <somethchan@gmail.com>
2026-02-28 09:41:32 -0800

    Add volume mount for mlperf_scripts in docker_run.sh

--- Docker/docker_run.sh
+++ Docker/docker_run.sh
@@ -5,5 +5,6 @@ docker run -it --gpus all \
   -v $(pwd)/mlperf_models:/models \
   -v $(pwd)/mlperf_results:/results \
   -v $(pwd)/mlperf_chace:/cache \
+  -v $(pwd)/mlperf_scripts:/scripts \
   -w /workspace \
   f9e7195e2020 bash
