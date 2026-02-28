c6a0e07e42234da93ebf351b69f41f8e58f2f38b
somethchan <somethchan@gmail.com>
2026-02-28 11:55:36 -0800

    Update cache volume path in docker_run.sh

--- Docker/docker_run.sh
+++ Docker/docker_run.sh
@@ -1,10 +1,11 @@
 docker run -it --gpus all \
   --cap-add=SYS_ADMIN \
   --security-opt seccomp=unconfined \
+  -e CUDA_VISIBLE_DEVICES=0 \
   -v $(pwd)/mlperf_data:/datasets \
   -v $(pwd)/mlperf_models:/models \
   -v $(pwd)/mlperf_results:/results \
-  -v $(pwd)/mlperf_chace:/cache \
+  -v $(pwd)/mlperf_cache:/cache \
   -v $(pwd)/mlperf_scripts:/scripts \
   -w /workspace \
-  9f9674976a12 bash
+  <IMG> bash
