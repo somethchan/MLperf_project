544ab198c0011afe5be3bcfe77edcd2971a767b5
somethchan <somethchan@gmail.com>
2026-03-04 06:26:46 -0800

    Setup Docker and NVIDIA container toolkit
    Added installation steps for Docker and NVIDIA container toolkit, including user permissions and environment setup.

--- Docker/prep_host.sh
+++ Docker/prep_host.sh
@@ -1,1 +1,28 @@
-mkdir mlperf_data mlperf_models mlperf_results mlperf_cache
+nvidia-smi
+
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+sudo apt-get install -y --no-install-recommends ca-certificates curl gnupg
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey \
+ | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list \
+ | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' \
+ | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list >/dev/null
+
+sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+sudo docker run --rm --gpus all nvidia/cuda:12.4.1-base-ubuntu22.04 nvidia-smi
+
+python3 -m pip install --user -U pip
+python3 -m pip install --user -U pip cmx4mlperf
+export PATH="$HOME/.local/bin:$PATH"
+
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+which cr
+cr --help | head
